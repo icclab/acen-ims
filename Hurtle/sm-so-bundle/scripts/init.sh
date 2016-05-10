329293424518354c7759d90b5b099b4d64682a3e
cima <cima@zhaw.ch>
2016-05-10 14:51:54 +0200

    [src] Hurtle SO/SM bundle: added example init / deploy / provision / delete scripts

--- Hurtle/sm-so-bundle/scripts/init.sh
+++ Hurtle/sm-so-bundle/scripts/init.sh
@@ -2,4 +2,9 @@ curl -v -X PUT http://localhost:8051/orchestrator/default \
 -H 'Content-Type: text/occi' \
 -H 'Category: orchestrator; scheme="http://schemas.mobile-cloud-networking.eu/occi/service#"' \
 -H 'X-Auth-Token: '$KID \
--H 'X-Tenant-Name: '$TENANT
+-H 'X-Tenant-Name: '$TENANT \
+-H 'X-OCCI-Attribute: acen.ims.platform="cs"' \
+-H 'X-OCCI-Attribute: acen.ims.region="CloudStack"' \
+-H 'X-OCCI-Attribute: acen.ims.dns_zone="example.com"' \
+-H 'X-OCCI-Attribute: acen.ims.dns_key="zPDgJ0y0AEmCP7fzCi93zfBRYRcYdDDv5xrmwyv7rLgzqSnBlT8n0o1mrHTNpety1QUK55+nBKAedcRluAW39w=="' \
+-H 'X-OCCI-Attribute: acen.ims.signup_key="mvPC5Zza"'
